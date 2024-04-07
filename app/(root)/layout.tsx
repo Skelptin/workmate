bee14698d7875867567eb74601a947e412f4d083
Mayank Painkra <mpainkra7@gmail.com>
2024-04-07 23:05:05 +0530

    ceated navbar and darkmode toggle

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,16 @@
+import { Navabr } from "@/components/navbar";
+
 const RootLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
-    return (  <div>
-        {children}
+    return (<div className="h-full">
+        <Navabr />
+        <main className="md:pl-20 pt-16 h-full">
+            {children}
+        </main>
+
     </div>);
 }
 
